2c68fca2233c1db1bf6c5b94caa9aa10e7a1f6f1
Wolfgang Beer <wolfgang.beer@dynatrace.com>
2022-07-06 16:12:18 +0200

    Update av_plugin.py

--- av_plugin.py
+++ av_plugin.py
@@ -20,10 +20,17 @@ class AvPlugin(BasePlugin):
         # we search for running process group with a given name
         pg_list = self.find_all_process_groups(pgi_name(PROCESS_NAME))
         if len(pg_list) < 1:
-            logger.info('Alarm process not found')
-            self.results_builder.report_availability_event(description = EVENT_DESCRIPTION, title = EVENT_TITLE, properties = {}, entity_selector = HostSelector())
+            logger.info('AvPlugin: Alarm process not found')
+            # extension framework just accepts string string properties!!
+            event_properties = {
+                "prop1" : "value1",
+                "dt.event.allow_davis_merge" : "false",
+                "dt.davis.analysis_trigger_delay" : "0",
+                "dt.davis.analysis_time_budget" : "0"
+            }
+            self.results_builder.report_availability_event(description = EVENT_DESCRIPTION, title = EVENT_TITLE, properties = event_properties, entity_selector = HostSelector())
     
-        logger.info('AvPlugin is executing: ' + str(len(pg_list)))
+        logger.info('AvPlugin: is executing: ' + str(len(pg_list)))
         
         
 
